8e890d526dc63a55145a2245bb184d61b7a4f6dc
burakkocak884 <burakkocak884@hotmail.com>
2020-08-06 23:19:49 -0400

    first commit

---
+++ Controllers/ListController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using mvc_project.Models;
+
+namespace mvc_project.Controllers
+{
+    public class ListController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public ListController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
